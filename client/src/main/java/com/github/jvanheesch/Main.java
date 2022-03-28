d26258dd9dbbd0bf276b04fba9c947a469882bf8
jvanheesch <joris.vanheesch@vdab.be>
2022-03-28 23:27:45 +0200

    add 3rd-party-jar-wrapper - unpack & filter classes from 3rd-party-jar - NOT a sub module of maven-exclude-classes-from-dependency to mitigate code completion errors in IntelliJ

--- client/src/main/java/com/github/jvanheesch/Main.java
+++ client/src/main/java/com/github/jvanheesch/Main.java
@@ -1,11 +1,12 @@
 package com.github.jvanheesch;
 
 import com.github.jvanheesch.package1.Class1;
-import com.github.jvanheesch.package2.Class2;
+// import com.github.jvanheesch.package2.Class2;
 
 public class Main {
     public static void main(String[] args) {
         Class1 class1 = new Class1();
-        Class2 class2 = new Class2();
+        // no longer compiles!
+        // Class2 class2 = new Class2();
     }
 }
